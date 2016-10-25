0614fa56cd58dd99377f45fc56012b34347e853d
Darin Etherington <etherind@oregonstate.edu>
2016-10-25 01:08:28 -0700

    Updated code for greedy algorithm file

--- changegreedy.py
+++ changegreedy.py
@@ -11,12 +11,16 @@
 	
 	A = amount of change to make
 	V = an array with the possible coin denominations (sorted)
-	C = an array with the toatal coins for each denomination
-	m = minimum coins used to make change
+	listMin = an array with the toatal coins for each denomination
+	coinMin = minimum coins used to make change
 	
 '''
 
+import sys
+import os
+
 def changegreedy(coinDenoms, changeToMake):
+	name = "changegreedy"
 	coinsUsed = []
 	coinsUsedHere = 0
 	coinsUsedSoFar = 0
@@ -29,16 +33,41 @@ def changegreedy(coinDenoms, changeToMake):
 		changeToMake = changeToMake - (coinsUsedHere * coinValue)
 		coinsUsedSoFar = coinsUsedSoFar + coinsUsedHere
 	
-	return coinsUsed, coinsUsedSoFar
+	return coinsUsed, coinsUsedSoFar, name
 
+def readFile(file):
+	with open(file) as f:
+		#remove list notation characters
+		lines = f.readline().replace('[','').replace(']','').rstrip('\n').strip(',')
+		lines2 = int(f.readline())
+	#remove commas
+	lines = map(int,lines.split(','))
+	return lines, lines2
+
+def writeFile(listMin,coinMin,file,name):
+	#find file name and file extension
+	base, ext = os.path.splitext(file)
+	#keep just file name
+	newFile = base + "change.txt"
+	with open(newFile,"a") as f:
+		f.write(name + ":")
+		f.write('\n')
+		f.write(str(listMin))
+		f.write('\n')
+		f.write(str(coinMin))
+		f.write('\n\n')
+	
 def main():
-	V = [1, 3, 7, 12] 
-	A = 29
+	#V = [1, 3, 7, 12] 
+	#A = 31
 	
-	C, m = changegreedy(V, A)
+	file = raw_input("Enter a file name: ")
+	V,A = readFile(file)
+	listMin, coinMin, algoName = changegreedy(V, A)
+	writeFile(listMin,coinMin,file,algoName)
 	
-	print C
-	print m
+	print listMin
+	print coinMin
 	
 if __name__ == "__main__":
 	main()
